6cb58a2d49e566d30c43058250cea0d1a3388c73
nurulmarisa <142717696+nurulmarisa8@users.noreply.github.com>
2025-05-12 19:21:19 +0800

    update

---
+++ TUPRAK7/appbook/models/Book.java
@@ -0,0 +1,13 @@
+package appbook.models;
+
+public class Book {
+    protected String author;
+    
+
+    public String pinjam(){
+        if (isBorrowed >= 14){
+            
+        }
+    }
+
+}
